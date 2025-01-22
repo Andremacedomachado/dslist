29e91ea2ef8c0c91a5813611cca0203adae6ee98
André Macedo Machado <37791756+Andremacedomachado@users.noreply.github.com>
2025-01-22 15:42:18 -0300

    Game seed

--- src/main/java/com/andremacedomachado/dslist/entities/Game.java
+++ src/main/java/com/andremacedomachado/dslist/entities/Game.java
@@ -15,27 +15,34 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
-    
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(long id, String title, Integer year, String genre, String plataform, String imgUrl,
+    public Game(long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = plataforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
+
         this.longDescription = longDescription;
     }
 
@@ -71,12 +78,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String plataforms) {
+        this.platforms = plataforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
